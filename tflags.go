f61c5e4ccb4191920d0c7da080506abba7912abc
atemmel <temmeladam@gmail.com>
2023-06-20 20:07:32 +0200

    Can now parse booleans

--- tflags.go
+++ tflags.go
@@ -1,5 +1,106 @@
 package tflags
 
-func Noop() bool {
-	return true
+import "os"
+
+type flag struct {
+	pbool *bool
+	pint *int
+	pstring *string
+}
+
+var(
+	flags = map[string]flag{}
+	unmatched = []string{}
+)
+
+type Meta struct {
+	Long string
+	Short string
+	//TODO: present help
+	Help string
+}
+
+func String(s *string, meta Meta) {
+	if meta.Short != "" {
+		flags["-" + meta.Short] = sflag(s)
+	}
+	if meta.Long != "" {
+		flags["--" + meta.Long] = sflag(s)
+	}
+}
+
+func Int(i *int, meta Meta) {
+	if meta.Short != "" {
+		flags["-" + meta.Short] = iflag(i)
+	}
+	if meta.Long != "" {
+		flags["--" + meta.Long] = iflag(i)
+	}
+}
+
+func Bool(b *bool, meta Meta) {
+	if meta.Short != "" {
+		flags["-" + meta.Short] = bflag(b)
+	}
+	if meta.Long != "" {
+		flags["--" + meta.Long] = bflag(b)
+	}
+}
+
+func Parse() {
+	ParseThem(os.Args)
+}
+
+func ParseThem(args []string) {
+	n := len(args)
+	for i := 0; i < n; i++ {
+		arg := args[i]
+		flag, ok := flags[arg]
+		if !ok {
+			unmatched = append(unmatched, arg)
+			continue
+		} 
+
+		if flag.pbool != nil {
+			*flag.pbool = true
+			continue
+		}
+
+		if flag.pstring != nil {
+			//TODO: range check
+		}
+
+		if flag.pint != nil {
+			//TODO: range check
+		}
+	}
+
+}
+
+func Unmatched() []string {
+	return unmatched
+}
+
+func bflag(b *bool) flag {
+	return flag{
+		pbool: b,
+		pint: nil,
+		pstring: nil,
+	}
+}
+
+func iflag(i *int) flag {
+	return flag{
+		pbool: nil,
+		pint: i,
+		pstring: nil,
+	}
+}
+
+func sflag(s *string) flag {
+	return flag{
+		pbool: nil,
+		pint: nil,
+		pstring: s,
+	}
 }
--- tflags_test.go
+++ tflags_test.go
@@ -2,8 +2,37 @@ package tflags
 
 import "testing"
 
-func TestNoop(t *testing.T) {
-	if(Noop()) {
-		t.Errorf("Whoopdiedo")
+func TestParse(t *testing.T) {
+
+	jamesFlag := false
+	bondFlag := false
+
+	Bool(&jamesFlag, Meta{Long: "james", Short: "j", Help: ""})
+	Bool(&bondFlag, Meta{Long: "bond", Short: "b", Help: ""})
+
+	args := []string{
+		"--james",
+		"is",
+		"-b",
+	}
+
+	ParseThem(args)
+
+	if !jamesFlag {
+		t.Errorf("jamesFlag was not true")
+	}
+
+	if !bondFlag {
+		t.Errorf("bondFlag was not true")
+	}
+
+	other := Unmatched()
+
+	if len(other) != 1 {
+		t.Errorf("no other flags caught")
+	}
+
+	if other[0] != "is" {
+		t.Errorf("did not catch 'is' flag")
 	}
 }
